c50de21035a014733005dc16ca5bc81353e2edbf
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-05-10 13:24:01 +0300

    deprecate empty-backend

---
+++ src/automata.app.test.js
@@ -0,0 +1,26 @@
+const supertest = require('supertest');
+
+const appBuilder = require('./app');
+
+let api;
+
+describe('automata-app', () => {
+  beforeAll(() => {
+    api = supertest(appBuilder({ db: global.client }));
+  });
+
+  it('should start', async () => {
+    const { status } = await api.get('/health');
+    expect(status).toBe(200);
+  });
+
+  it('should create and authenticate users', async () => {
+    const credendials = { email: `foo${Math.random()}@example.com`, password: 'bar' };
+    const { status: signupStatus } = await api.post('/signup').send(credendials);
+    expect(signupStatus).toBe(201);
+
+    const { status, body } = await api.post('/login').send(credendials);
+    expect(status).toBe(200);
+    expect(body.token).toBeTruthy();
+  });
+});
