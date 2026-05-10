4945837a127671bf763c768aa7721bad5768403c
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-05-10 12:08:24 +0300

    use app builder api

--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ const { SECRET, EMAIL_VERIFICATION_SECRET } = require('./config');
 // const router = require('./media-library-server/router');
 
 module.exports = ({ db }) => {
+<<<<<<< HEAD
   const app = appBuilder({ db, EMAIL_VERIFICATION_SECRET, SECRET });
 
   // app.use(router({ db }));
+=======
+  const app = appBuilder({ db });
+
+  app.use(router({ db }));
+>>>>>>> 2f563e1 (use app builder api)
 
   return app;
 };
