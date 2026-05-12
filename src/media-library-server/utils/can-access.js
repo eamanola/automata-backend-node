858282d84c0e07d037cf84b31237d2976f1e7117
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-05-12 14:49:49 +0300

    remove  media-library-server code

--- src/media-library-server/utils/can-access.js
+++
@@ -1,5 +0,0 @@
-const { MEDIA_LIBS } = require('../config');
-
-const canAccess = (path) => MEDIA_LIBS.some((mediaLib) => path.startsWith(mediaLib));
-
-module.exports = canAccess;
--- src/media-library-server/utils/escape-path.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line prefer-named-capture-group
-const escapePath = (path) => path.replace(/([`])/ug, '\\$1');
-
-module.exports = escapePath;
