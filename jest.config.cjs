e4738e5065f972155a8f73acc5a474d0673c7c34
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-05-15 15:06:06 +0300

    make some packages optional

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,4 @@
 module.exports = {
-  setupFiles: [
-    './jest/jest.setup.config.js',
-    './jest/jest.setup.mongodb-memory-server.js',
-    './jest/jest.setup.redis.js',
-  ],
+  setupFiles: ['./jest/jest.setup.config.js', './jest/jest.setup.redis.js'],
   setupFilesAfterEnv: ['./jest/jest.setup.db.js', './jest/jest.setup.cache.js'],
 };
