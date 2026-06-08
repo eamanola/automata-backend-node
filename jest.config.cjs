f6b2e0fe4ef266d73cd557d0f20ac1dff14a0d47
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-06-08 15:23:58 +0300

    bring it all together

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,23 @@
+// <rootDir> confs
 module.exports = {
+  // exclude from <rootDir> project test
+  modulePathIgnorePatterns: [
+    '<rootDir>/automata/automata-app',
+    '<rootDir>/automata/automata-cache',
+    '<rootDir>/automata/automata-db',
+    '<rootDir>/automata/automata-rest',
+    '<rootDir>/automata/automata-user-management',
+    '<rootDir>/automata/automata-utils',
+  ],
+  projects: [
+    '<rootDir>',
+    '<rootDir>/automata/automata-app',
+    '<rootDir>/automata/automata-cache',
+    '<rootDir>/automata/automata-db',
+    '<rootDir>/automata/automata-rest',
+    '<rootDir>/automata/automata-user-management',
+    '<rootDir>/automata/automata-utils',
+  ],
   setupFiles: ['./jest/jest.setup.config.js', './jest/jest.setup.redis.js'],
   setupFilesAfterEnv: ['./jest/jest.setup.db.js', './jest/jest.setup.cache.js'],
 };
