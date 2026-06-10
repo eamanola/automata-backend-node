1ba41a29b734e73dd66277b162ecf9246d1948b8
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-06-10 17:43:20 +0300

    update many test

--- src/automata.rest.test.js
+++ src/automata.rest.test.js
@@ -33,6 +33,22 @@ describe('automata-rest', () => {
     expect(get2.status).toBe(200);
     expect(get2.body.result).toEqual(expect.objectContaining({ foo: 'baz', id }));
 
+    // update many
+    const post2 = await api.post('/test').send({ foo: 'bar' });
+    const { result: created2 } = post2.body;
+    const { id: id2 } = created2;
+
+    const put2 = await api.put('/test').send([
+      { ...created, foo: 'baz2' },
+      { ...created2, foo: 'baz3' },
+    ]);
+    const { results } = put2.body;
+    expect(results.find(({ id: aId }) => aId === id))
+      .toEqual(expect.objectContaining({ foo: 'baz2', id }));
+    expect(results.find(({ id: aId }) => aId === id2))
+      .toEqual(expect.objectContaining({ foo: 'baz3', id: id2 }));
+    // update many
+
     await db.dropTable(table.name);
   });
 });
